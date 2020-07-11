ba8281ccd120ed1684f4b2a1c8d4f4ef5ad92ed9
Philip Schulz <phs@phisch.org>
2020-07-11 14:54:58 +0200

    Update README.md and get rid of STM32L4 left-overs.

--- main.cpp
+++ main.cpp
@@ -22,37 +22,6 @@
 /*******************************************************************************
  * System Devices
  ******************************************************************************/
-#if defined(STM32L432xx)
-static const constexpr devices::PllConfigurationValuesT<devices::Stm32L4xx> pllCfgValues = {
-    .m_pllSource = devices::Stm32L4xx::PllSource_t::e_PllSourceMSI,
-
-    .m_msiRange = devices::Stm32L4xx::MSIRange_t::e_MSIRange_4MHz,
-    .m_hseSpeedInHz = 0,
-
-    .m_pllM = 1,
-    
-    /* Main PLL */
-    .m_pllN = 40,
-
-    .m_pllP = devices::Stm32L4xx::PllP_t::e_PllP_Disabled,
-    .m_pllQ = devices::Stm32L4xx::PllQ_t::e_PllQ_Disabled,
-    .m_pllR = devices::Stm32L4xx::PllR_t::e_PllR_Div2,
-
-    /* Serial Audio Interface (SAI) PLL */
-    .m_pllSaiN  = 24,
-    .m_pllSaiP  = devices::Stm32L4xx::PllP_t::e_PllP_Disabled,
-    .m_pllSaiQ  = devices::Stm32L4xx::PllQ_t::e_PllQ_Disabled,
-    .m_pllSaiR  = devices::Stm32L4xx::PllR_t::e_PllR_Div2,
-
-    /* System and AHP / APB Bus Clocks */
-    .m_sysclkSource = devices::Stm32L4xx::SysclkSource_t::e_SysclkPLL,
-    .m_ahbPrescaler = devices::Stm32L4xx::AHBPrescaler_t::e_AHBPrescaler_None,
-    .m_apb1Prescaler = devices::Stm32L4xx::APBPrescaler_t::e_APBPrescaler_None,
-    .m_apb2Prescaler = devices::Stm32L4xx::APBPrescaler_t::e_APBPrescaler_None,
-};
-#endif
-
-#if defined(STM32F407xx)
 static const constexpr devices::PllConfigurationValuesT<devices::Stm32F407xx> pllCfgValues = {
     .m_pllSource        = devices::Stm32F407xx::PllSource_t::e_PllSourceHSE,
     .m_hseSpeedInHz     = 8 * 1000 * 1000,
@@ -65,7 +34,6 @@ static const constexpr devices::PllConfigurationValuesT<devices::Stm32F407xx> pl
     .m_apb1Prescaler    = devices::Stm32F407xx::APBPrescaler_t::e_APBPrescaler_Div4,
     .m_apb2Prescaler    = devices::Stm32F407xx::APBPrescaler_t::e_APBPrescaler_Div2
 };
-#endif
 
 static const constexpr devices::PllConfigurationInterfaceT<decltype(pllCfgValues)> pllCfg(pllCfgValues);
 
@@ -78,15 +46,6 @@ static devices::NvicViaSTM32F4          nvic(NVIC, scb);
 /*******************************************************************************
  * GPIO Engine Handlers 
  ******************************************************************************/
-#if defined(STM32L432xx)
-static gpio::GpioAccessViaSTM32F4_GpioA gpio_A(rcc);
-static gpio::GpioEngine                 gpio_engine_A(&gpio_A);
-
-static gpio::GpioAccessViaSTM32F4_GpioB gpio_B(rcc);
-static gpio::GpioEngine                 gpio_engine_B(&gpio_B);
-#endif
-
-#if defined(STM32F407xx)
 static gpio::GpioAccessViaSTM32F4_GpioA gpio_A(rcc);
 static gpio::GpioEngine                 gpio_engine_A(&gpio_A);
 
@@ -95,37 +54,20 @@ static gpio::GpioEngine                 gpio_engine_C(&gpio_C);
 
 static gpio::GpioAccessViaSTM32F4_GpioD gpio_D(rcc);
 static gpio::GpioEngine                 gpio_engine_D(&gpio_D);
-#endif
 
 /*******************************************************************************
  * LEDs
  ******************************************************************************/
-#if defined(STM32L432xx)
-static gpio::PinT<decltype(gpio_engine_B)>  g_led_green(&gpio_engine_B, 3);
-static gpio::PinT<decltype(gpio_engine_A)>  g_mco(&gpio_engine_A, 8);
-#endif
-
-#if defined(STM32F407xx)
 static gpio::PinT<decltype(gpio_engine_A)>  g_mco1(&gpio_engine_A, 8);
 static gpio::PinT<decltype(gpio_engine_D)>  g_led_green(&gpio_engine_D, 12);
-#endif
 
 /*******************************************************************************
  * UART
  ******************************************************************************/
-#if defined(STM32F407xx)
 static gpio::PinT<decltype(gpio_engine_C)>  uart_tx(&gpio_engine_C, 6);
 static gpio::PinT<decltype(gpio_engine_C)>  uart_rx(&gpio_engine_C, 7);
 static uart::UartAccessSTM32F4_Uart6        uart_access(rcc, uart_rx, uart_tx);
 uart::UartDevice                            g_uart(&uart_access);
-#endif
-
-#if defined(STM32L432xx)
-static gpio::PinT<decltype(gpio_engine_A)>  uart_tx(&gpio_engine_A, 2);
-static gpio::PinT<decltype(gpio_engine_A)>  uart_rx(&gpio_engine_A, 15);
-static uart::UartAccessSTM32F4_Uart2        uart_access(rcc, uart_rx, uart_tx);
-uart::UartDevice                            g_uart(&uart_access);
-#endif
 
 /*******************************************************************************
  * Tasks
@@ -146,23 +88,12 @@ extern char bstack, estack;
 
 const uint32_t SystemCoreClock = pllCfg.getSysclkSpeedInHz();
 
-#if defined(STM32L432xx)
-static_assert(SystemCoreClock == 80 * 1000 * 1000);
-
-static_assert(SystemCoreClock               ==  80 * 1000 * 1000,   "Expected System Clock to be at 80 MHz!");
-static_assert(pllCfg.getAhbSpeedInHz()      ==  80 * 1000 * 1000,   "Expected AHB to be running at 80 MHz!");
-static_assert(pllCfg.getApb1SpeedInHz()     ==  80 * 1000 * 1000,   "Expected APB1 to be running at 80 MHz!");
-static_assert(pllCfg.getApb2SpeedInHz()     ==  80 * 1000 * 1000,   "Expected APB2 to be running at 80 MHz!");
-#endif
-
-#if defined(STM32F407xx)
 static_assert(pllCfg.isValid(pllCfg) == true,                       "PLL Configuration is not valid!");
 
 static_assert(SystemCoreClock               == 168 * 1000 * 1000,   "Expected System Clock to be at 168 MHz!");
 static_assert(pllCfg.getAhbSpeedInHz()      == 168 * 1000 * 1000,   "Expected AHB to be running at 168 MHz!");
 static_assert(pllCfg.getApb1SpeedInHz()     ==  42 * 1000 * 1000,   "Expected APB1 to be running at 42 MHz!");
 static_assert(pllCfg.getApb2SpeedInHz()     ==  84 * 1000 * 1000,   "Expected APB2 to be running at 84 MHz!");
-#endif
 
 #if defined(__cplusplus)
 } /* extern "C" */
@@ -179,16 +110,9 @@ void
 main(void) {
     g_led_green.enable(gpio::GpioAccessViaSTM32F4::e_Output, gpio::GpioAccessViaSTM32F4::e_None, gpio::GpioAccessViaSTM32F4::e_Gpio);
 
-#if defined(STM32F407xx)
     rcc.setMCO<decltype(g_mco1), gpio::GpioAccessViaSTM32F4>(g_mco1, decltype(rcc)::MCO1Output_e::e_PLL, decltype(rcc)::MCOPrescaler_t::e_MCOPre_5);
-#endif
-
-#if defined(STM32L432xx)
-    rcc.setMCO<decltype(g_mco), gpio::GpioAccessViaSTM32F4>(g_mco, decltype(rcc)::MCOOutput_e::e_PLL, decltype(rcc)::MCOPrescaler_t::e_MCOPre_4);
-#endif
 
     uart_access.setBaudRate(decltype(uart_access)::e_BaudRate_115200);
-    // uart_access.setBaudRate(decltype(uart_access)::e_BaudRate_230400);
 
     g_uart.printf("Copyright (c) 2013-2020 Philip Schulz <phs@phisch.org>\r\n");
     g_uart.printf("All rights reserved.\r\n");
